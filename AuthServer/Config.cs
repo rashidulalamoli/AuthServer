0161b0f29826c1cefad638b02d3a3bb0dba9323d
oli <ra@hoxro.com>
2019-10-21 18:10:50 +0600

    gg

---
+++ AuthServer/Config.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4;
+
+namespace AuthServer
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                 new ApiResource("hoxrofinance", "HoxroFinance")
+                {
+                    Scopes = {new Scope("api.read")}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client {
+                    RequireConsent = false,
+                    ClientId = "angular_spa",
+                    ClientName = "Angular SPA",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "api.read"
+                    },
+                    RedirectUris = {"http://localhost:4200/auth-callback"},
+                    PostLogoutRedirectUris = {"http://localhost:4200/?postLogout=true"},
+                    AllowedCorsOrigins = {"http://localhost:4200"},
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 3600
+                }
+            };
+        }
+    }
+}
